2b422de17ec55b126f3d8b64d363b5ec22e0e268
tikuram111984 <tikuram111984@gmail.com>
2026-08-16 19:14:56 +0530

    main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ def runpolling():
 
 threading.Thread(target=runpolling, daemon=True).start()
 
-if name == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
     
